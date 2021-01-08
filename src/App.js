a976febe45a0e5a5010ec70c754f626fc9ca9060
Teddy-Hung <tedhungkk@gmail.com>
2021-01-07 17:27:57 -0800

    added mobile media query

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import react, {Component} from 'react'
+import React, {Component} from 'react'
 import './App.css';
 
 class App extends Component {
@@ -17,34 +17,35 @@ class App extends Component {
   render(){
     return (
       <div className='App'>
-        <head>
-          <meta charset="UTF-8"></meta>
-          <meta name="description" content="responsive-design-clone"></meta>
-          <meta name="keywords" content="responsive-design-clone, devmtn"></meta>
-          <meta name='Author' content='Teddy Hung WR8'></meta>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-        </head>
-        <body>
-          <nav className='App-header'>
-            <section className='Header-nav-bar'>
-              <img src='https://startbootstrap.github.io/startbootstrap-agency/assets/img/navbar-logo.svg' alt='logo'></img>
-              <section className='Desktop-menu'>
-                <span>SERVICES</span>
-                <span>PORTFOLIO</span>
-                <span>ABOUT</span>
-                <span>TEAM</span>
-                <span>CONTACT</span>
-              </section>
+        <nav className='App-header'>
+          <section className='Header-nav-bar'>
+            <img src='https://startbootstrap.github.io/startbootstrap-agency/assets/img/navbar-logo.svg' alt='logo'></img>
+            <section className='Desktop-menu'>
+              <span id='services'>SERVICES</span>
+              <span id='portfolio'>PORTFOLIO</span>
+              <span id='about'>ABOUT</span>
+              <span id='team'>TEAM</span>
+              <span id='contact'>CONTACT</span>
             </section>
-            <main class="Header-main">
-              <span>Welcome To Our Studio!</span>
-              <span>IT'S NICE TO MEET YOU</span>
-              <button>TELL ME MORE</button>
-            </main>
-          </nav>
-
-        </body>
-
+            <button className='Mobile-menu-button' onClick={this.toggleDropdown}>MENU</button>
+            {this.state.dropdownView
+              ? (
+                <nav className='Mobile-menu'>
+                  <span id='services'>SERVICES</span>
+                  <span id='portfolio'>PORTFOLIO</span>
+                  <span id='about'>ABOUT</span>
+                  <span id='team'>TEAM</span>
+                  <span id='contact'>CONTACT</span>
+                </nav>
+              )
+              : null}
+          </section>
+          <main class="Header-main">
+            <span id='text1'>Welcome To Our Studio!</span>
+            <span id='text2'>IT'S NICE TO MEET YOU</span>
+            <button id='info-button'>TELL ME MORE</button>
+          </main>
+        </nav>
       </div>
     )
   }
